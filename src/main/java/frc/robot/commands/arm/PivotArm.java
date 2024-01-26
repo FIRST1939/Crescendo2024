32d37db34c78bb191cb0baf1c3f9b541c401e3a8
PhobicSugar6029 <eshanavik@icloud.com>
2024-01-25 19:32:38 -0600

    Arm and Shooter Commands

---
+++ src/main/java/frc/robot/commands/arm/PivotArm.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.arm;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Arm;
+
+public class PivotArm extends Command {
+    
+    private Arm arm; 
+
+    public PivotArm (Arm arm) {
+
+        this.arm = arm;
+        this.addRequirements(this.arm);
+    }
+}
