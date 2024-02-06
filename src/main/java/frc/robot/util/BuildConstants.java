4f490357cac38b564a0e1a8caf8a601aed25f837
1939DriverStation <barstowrobotics1939@gmail.com>
2024-02-05 19:50:21 -0600

    Auton Updates
    Two working working paths

--- src/main/java/frc/robot/util/BuildConstants.java
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 28;
-  public static final String GIT_SHA = "7ecfedb9f9ec6956f223ed1345f61be6542e61e5";
-  public static final String GIT_DATE = "2024-02-03 15:24:32 CST";
+  public static final int GIT_REVISION = 29;
+  public static final String GIT_SHA = "299ebcb2fb9ede66a382139699c569b9667231d3";
+  public static final String GIT_DATE = "2024-02-03 15:28:23 CST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2024-02-03 15:25:49 CST";
-  public static final long BUILD_UNIX_TIME = 1706995549712L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2024-02-05 19:34:58 CST";
+  public static final long BUILD_UNIX_TIME = 1707183298432L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
