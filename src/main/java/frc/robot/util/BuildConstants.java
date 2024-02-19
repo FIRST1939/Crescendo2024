a2db662a4859a837fc94c2aecbe38f7521cd2c69
1939DriverStation <barstowrobotics1939@gmail.com>
2024-02-19 16:55:57 -0600

    SysidRoutine Data

--- src/main/java/frc/robot/util/BuildConstants.java
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 43;
-  public static final String GIT_SHA = "f815f53c775e5015a02ca2ec9e84a0ac6e1c3794";
-  public static final String GIT_DATE = "2024-02-19 16:07:09 CST";
+  public static final int GIT_REVISION = 44;
+  public static final String GIT_SHA = "d9716a96e9425c35246a9fca97f3026dbf49385a";
+  public static final String GIT_DATE = "2024-02-19 16:36:37 CST";
   public static final String GIT_BRANCH = "prototypeControls";
-  public static final String BUILD_DATE = "2024-02-19 16:35:23 CST";
-  public static final long BUILD_UNIX_TIME = 1708382123527L;
+  public static final String BUILD_DATE = "2024-02-19 16:54:17 CST";
+  public static final long BUILD_UNIX_TIME = 1708383257862L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
