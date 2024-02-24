ee9a384eb10339ba53188811506fcafc0ceadc06
StarbuckBarista <carlitos.salazar@protonmail.com>
2024-02-24 10:51:46 -0600

    Arm PID Control

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -2,10 +2,10 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
-import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.Voltage;
@@ -22,6 +22,7 @@ public class Arm extends SubsystemBase {
     
     private TalonFX pivot;
     private DutyCycleEncoder pivotEncoder;
+    private PIDController pivotController;
 
     private DoubleSupplier pivotPosition;
     private DoubleSupplier pivotVelocity;
@@ -43,24 +44,29 @@ public class Arm extends SubsystemBase {
         this.pivot.getMotorVoltage().setUpdateFrequency(50);
         this.pivot.getTorqueCurrent().setUpdateFrequency(50);
 
-        this.pivotPosition = () -> this.pivot.getPosition().getValue() * Constants.ArmConstants.PIVOT_REDUCTION;
-        this.pivotVelocity = () -> this.pivot.getVelocity().getValue() * Constants.ArmConstants.PIVOT_REDUCTION;
+        this.pivotController = new PIDController(
+            Constants.ArmConstants.PIVOT_P,
+            Constants.ArmConstants.PIVOT_I,
+            Constants.ArmConstants.PIVOT_D
+        );
+
+        this.pivotPosition = () -> this.pivotEncoder.get() * 360;
+        this.pivotVelocity = () -> this.pivot.getVelocity().getValue() * Constants.ArmConstants.PIVOT_REDUCTION * 360;
 
         this.lowerBound = new DigitalInput(Constants.ArmConstants.LOWER_BOUND);
         this.upperBound = new DigitalInput(Constants.ArmConstants.UPPER_BOUND);
     }
 
-    @Override
-    public void periodic () {
+    public void setPosition (double position) { 
+        
+        double input = this.pivotController.calculate(this.pivotPosition.getAsDouble(), position);
+        if (input < 0.0 && this.lowerBound.get()) input = 0.0;
+        if (input > 0.0 && this.upperBound.get()) input = 0.0;
 
-        if (this.lowerBound.get() || this.upperBound.get()) {
-
-            this.pivot.stopMotor();
-        }
+        this.pivot.set(input);
     }
 
-    public void setPosition (double position) { this.pivot.setControl(new PositionVoltage(position)); }
-    public boolean atPosition () { return true; }
+    public boolean atPosition () { return this.pivotController.atSetpoint(); }
 
     public Command getQuasistaticRoutine (Direction direction) { return this.getSysIdRoutine().quasistatic(direction); }
     public Command getDynamicRoutine (Direction direction) { return this.getSysIdRoutine().dynamic(direction); }
