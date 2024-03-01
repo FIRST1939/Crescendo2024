54ce5fd2fb2a7070aefd7ca67432a1ab20204725
StarbuckBarista <carlitos.salazar@protonmail.com>
2024-03-01 08:53:30 -0600

    Subsystem Cleanup

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -1,21 +1,13 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
-import edu.wpi.first.units.Measure;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.units.Voltage;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Mechanism;
 import frc.robot.util.Constants;
 import frc.robot.util.Constants.IdleBehavior;
 import frc.robot.util.MotorUtil;
@@ -43,14 +35,6 @@ public class Indexer extends SubsystemBase {
 
         this.backRollers.getEncoder().setPositionConversionFactor(Constants.IndexerConstants.BACK_ROLLERS_REDUCTION * (Math.PI * Constants.IndexerConstants.BACK_ROLLERS_DIAMETER));
         this.backRollers.getEncoder().setVelocityConversionFactor((Constants.IndexerConstants.BACK_ROLLERS_REDUCTION / 60.0) * (Math.PI * Constants.IndexerConstants.BACK_ROLLERS_DIAMETER));
-
-        this.frontRollers.getPIDController().setP(Constants.IndexerConstants.FRONT_ROLLERS_P);
-        this.frontRollers.getPIDController().setI(Constants.IndexerConstants.FRONT_ROLLERS_I);
-        this.frontRollers.getPIDController().setD(Constants.IndexerConstants.FRONT_ROLLERS_D);
-
-        this.backRollers.getPIDController().setP(Constants.IndexerConstants.BACK_ROLLERS_P);
-        this.backRollers.getPIDController().setI(Constants.IndexerConstants.BACK_ROLLERS_I);
-        this.backRollers.getPIDController().setD(Constants.IndexerConstants.BACK_ROLLERS_D);
 
         this.startBeam = new DigitalInput(Constants.IndexerConstants.START_BEAM);
         this.endBeam = new DigitalInput(Constants.IndexerConstants.END_BEAM);
@@ -130,49 +114,4 @@ public class Indexer extends SubsystemBase {
             this.backRollers.setIdleMode(IdleMode.kBrake);
         }
     }
-
-    public Command getFrontQuasistaticRoutine (Direction direction) { return this.getFrontSysIdRoutine().quasistatic(direction); }
-    public Command getFrontDynamicRoutine (Direction direction) { return this.getFrontSysIdRoutine().dynamic(direction); }
-    
-    public Command getBackQuasistaticRoutine (Direction direction) { return this.getBackSysIdRoutine().quasistatic(direction); }
-    public Command getBackDynamicRoutine (Direction direction) { return this.getBackSysIdRoutine().dynamic(direction); }
-
-    private SysIdRoutine getFrontSysIdRoutine () {
-
-        return new SysIdRoutine(
-            Constants.IndexerConstants.FRONT_SYSID_ROUTINE_CONFIG,
-            new Mechanism(
-                this::setFrontRollersVoltage,
-                sysIdRoutineLog -> {
-
-                    sysIdRoutineLog.motor("indexer-front-rollers")
-                        .linearPosition(Units.Inches.of(this.frontRollers.getEncoder().getPosition()))
-                        .linearVelocity(Units.InchesPerSecond.of(this.frontRollers.getEncoder().getVelocity()))
-                        .voltage(Units.Volts.of(this.frontRollers.getBusVoltage() * this.frontRollers.getAppliedOutput()));
-                },
-                this
-            )
-        );
-    }
-
-    private SysIdRoutine getBackSysIdRoutine () {
-
-        return new SysIdRoutine(
-            Constants.IndexerConstants.BACK_SYSID_ROUTINE_CONFIG,
-            new Mechanism(
-                this::setBackRollersVoltage,
-                sysIdRoutineLog -> {
-
-                    sysIdRoutineLog.motor("indexer-back-rollers")
-                        .linearPosition(Units.Inches.of(this.backRollers.getEncoder().getPosition()))
-                        .linearVelocity(Units.InchesPerSecond.of(this.backRollers.getEncoder().getVelocity()))
-                        .voltage(Units.Volts.of(this.backRollers.getBusVoltage() * this.backRollers.getAppliedOutput()));
-                },
-                this
-            )
-        );
-    }
-
-    private void setFrontRollersVoltage (Measure<Voltage> voltage) { this.frontRollers.getPIDController().setReference(voltage.magnitude(), ControlType.kVoltage); }
-    private void setBackRollersVoltage (Measure<Voltage> voltage) { this.backRollers.getPIDController().setReference(voltage.magnitude(), ControlType.kVoltage); }
 }
