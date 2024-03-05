df0e51576d41e288178f082673a26fd139634c91
StarbuckBarista <carlitos.salazar@protonmail.com>
2024-03-04 18:42:43 -0600

    Path Fixes

--- src/main/java/frc/robot/util/BuildConstants.java
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 72;
-  public static final String GIT_SHA = "bebf6f18946af9d926d5d57765ccbc0b47a1773c";
-  public static final String GIT_DATE = "2024-02-25 01:23:42 CST";
+  public static final int GIT_REVISION = 82;
+  public static final String GIT_SHA = "502f3e2431d352f23c9aee46a009904283e7f961";
+  public static final String GIT_DATE = "2024-03-04 17:47:29 CST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2024-02-25 01:27:29 CST";
-  public static final long BUILD_UNIX_TIME = 1708846049462L;
+  public static final String BUILD_DATE = "2024-03-04 18:42:21 CST";
+  public static final long BUILD_UNIX_TIME = 1709599341505L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
