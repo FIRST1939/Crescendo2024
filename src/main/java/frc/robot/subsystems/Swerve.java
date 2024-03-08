a7164a7edc69325d6a90135d1544c7c800bde7c2
StarbuckBarista <carlitos.salazar@protonmail.com>
2024-03-07 18:51:27 -0600

    Conflict Fixes

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -15,13 +15,10 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.util.Constants;
 import frc.robot.util.Constants.IdleBehavior;
 import swervelib.SwerveDrive;
-import swervelib.SwerveDriveTest;
 import swervelib.SwerveModule;
 import swervelib.parser.PIDFConfig;
 import swervelib.parser.SwerveDriveConfiguration;
@@ -120,35 +117,4 @@ public class Swerve extends SubsystemBase {
     }
 
     public void lock () { this.swerveDrive.lockPose(); }
-    
-    public Command getDriveSysidRoutine () {
-
-        SysIdRoutine sysIdRoutine = SwerveDriveTest.setDriveSysIdRoutine(
-            Constants.SwerveConstants.DRIVE_SYSID_CONFIG,
-            this, this.swerveDrive,
-            12.0
-        );
-
-        return SwerveDriveTest.generateSysIdCommand(
-            sysIdRoutine, 
-            Constants.SwerveConstants.DRIVE_SYSID_CONFIG.m_timeout.magnitude(), 
-            Constants.SwerveConstants.DRIVE_SYSID_QUASISTATIC_TIMEOUT, 
-            Constants.SwerveConstants.DRIVE_SYSID_DYNAMIC_TIMEOUT
-        );
-    }
-
-    public Command getAngleSysidRoutine () {
-
-        SysIdRoutine sysIdRoutine = SwerveDriveTest.setAngleSysIdRoutine(
-            Constants.SwerveConstants.ANGLE_SYSID_CONFIG,
-            this, this.swerveDrive
-        );
-
-        return SwerveDriveTest.generateSysIdCommand(
-            sysIdRoutine, 
-            Constants.SwerveConstants.ANGLE_SYSID_CONFIG.m_timeout.magnitude(), 
-            Constants.SwerveConstants.ANGLE_SYSID_QUASISTATIC_TIMEOUT, 
-            Constants.SwerveConstants.ANGLE_SYSID_DYNAMIC_TIMEOUT
-        );
-    }
 }
